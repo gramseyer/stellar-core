0442312ddbd24c93ff91cd8714d6c1ebc90d0cef
Geoff Ramseyer <gdwr@cs.stanford.edu>
2021-08-31 19:26:20 -0700

    orderbook query tests

--- src/speedex/test/IOCOrderbookTests.cpp
+++ src/speedex/test/IOCOrderbookTests.cpp
@@ -80,3 +80,39 @@ TEST_CASE("one offer", "[speedex]")
 	REQUIRE(orderbook.getPriceCompStats(1, 100).cumulativeOfferedForSaleTimesPrice == 0);
 }
 
+TEST_CASE("offers at identical price point", "[speedex]")
+{
+	IOCOrderbook orderbook(genericAssetPair());
+
+	int64_t amount = 10000;
+
+	addOffer(orderbook, 100, 100, amount, 1);
+	addOffer(orderbook, 200, 200, amount, 2);
+	addOffer(orderbook, 300, 300, amount, 3);
+	addOffer(orderbook, 100, 200, amount, 4);
+	addOffer(orderbook, 200, 100, amount, 5);
+
+	orderbook.doPriceComputationPreprocessing();
+
+	int128_t expectedHalfAmount = ((int128_t) amount) << 31;
+
+	REQUIRE(orderbook.getPriceCompStats(1, 1).cumulativeOfferedForSale == 4 * amount);
+	REQUIRE(orderbook.getPriceCompStats(1, 1).cumulativeOfferedForSaleTimesPrice == expectedHalfAmount + (((int128_t) (3 * amount)) << 32));
+
+
+	REQUIRE(orderbook.getPriceCompStats(101, 100).cumulativeOfferedForSale == 4 * amount);
+	REQUIRE(orderbook.getPriceCompStats(101, 100).cumulativeOfferedForSaleTimesPrice == expectedHalfAmount + (((int128_t) (3 * amount)) << 32));
+
+	REQUIRE(orderbook.getPriceCompStats(1, 2).cumulativeOfferedForSale == amount);
+	REQUIRE(orderbook.getPriceCompStats(1, 2).cumulativeOfferedForSaleTimesPrice == expectedHalfAmount);
+
+	REQUIRE(orderbook.getPriceCompStats(1, 200).cumulativeOfferedForSale == 0);
+	REQUIRE(orderbook.getPriceCompStats(1, 200).cumulativeOfferedForSaleTimesPrice == ((int128_t) 0) << 32);
+
+	REQUIRE(orderbook.getPriceCompStats(200, 100).cumulativeOfferedForSale == 5 * amount);
+	REQUIRE(orderbook.getPriceCompStats(200, 100).cumulativeOfferedForSaleTimesPrice == expectedHalfAmount + (((int128_t) (5 * amount)) << 32));
+
+	REQUIRE(orderbook.getPriceCompStats(201, 100).cumulativeOfferedForSale == 5 * amount);
+	REQUIRE(orderbook.getPriceCompStats(201, 100).cumulativeOfferedForSaleTimesPrice == expectedHalfAmount + (((int128_t) (5 * amount)) << 32));
+}
+
